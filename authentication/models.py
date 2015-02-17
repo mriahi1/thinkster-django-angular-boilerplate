af90e3c8980c2948a595c976947723cabf03f8fb
Maxime Riahi <maxriahi@gmail.com>
2015-02-17 13:49:25 -0500

    added authentication manageraccount and serializer

--- authentication/models.py
+++ authentication/models.py
@@ -1,30 +1,54 @@
-from django.contrib.auth.models import abstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-class Account(abstractBaseUser):
-	email = models.CharField(max_length=40, unique=True)
-	username = models.CharField(max_length=40, unique=True)
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('Users must have a valid email address.')
 
-	first_name = models.CharField(max_length=40, blank=True)
-	last_name = models.CharField(max_length=40, blank=True)
-	tagline = models.CharField(max_length=40, blank=True)
+        if not kwargs.get('username'):
+            raise ValueError('Users must have a valid username.')
 
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+        account = self.model(
+            email=self.normalize_email(email), username=kwargs.get('username')
+        )
 
-	is_admin = models.BooleanField(default=False)
+        account.set_password(password)
+        account.save()
 
-	objects = AccountManager()
+        return account
 
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELD = ['username']
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
 
-	def __unicode__(self):
-		return self.email
+        account.is_admin = True
+        account.save()
 
-	def get_full_name(self):
-		return ' '.join([self.first_name, self.last_name])
+        return account
 
-	def get_short_name(self):
-		return self.first_name
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
 
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=140, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __unicode__(self):
+        return self.email
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+    def get_short_name(self):
+        return self.first_name
